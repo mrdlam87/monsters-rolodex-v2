a079c6b3ecb985ca8c37a3b627a2a54d806eec5b
Danny Lam <dannny.lam@gmail.com>
2022-09-29 12:43:54 +1000

    update middlewares for development mode only

--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import monstersReducer from "./monsters.slice";
 import { loggerMiddleware } from "./logger";
 
+const middleWares = [
+  process.env.NODE_ENV !== "production" && loggerMiddleware,
+].filter(Boolean);
+
 export const store = configureStore({
   reducer: {
     monstersSlice: monstersReducer,
   },
-  middleware: [loggerMiddleware],
+  middleware: middleWares,
 });
