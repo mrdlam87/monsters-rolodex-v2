c9e58748b20c91d1288ba6170cedb4ba8ba82b1a
Danny Lam <dannny.lam@gmail.com>
2022-09-29 12:35:37 +1000

    add logging middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import monstersReducer from "./monsters.slice";
+import { loggerMiddleware } from "./logger";
 
 export const store = configureStore({
   reducer: {
     monstersSlice: monstersReducer,
   },
+  middleware: [loggerMiddleware],
 });
